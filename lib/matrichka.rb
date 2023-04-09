34a8a66f3b0778dee38e821ccb6ea95cc98219b6
sollaren4 <sollaren4@gmail.com>
2023-04-10 01:24:44 +0300

    Dev Matr Class

--- lib/matrichka.rb
+++ lib/matrichka.rb
@@ -6,3 +6,34 @@ module Matrichka
   class Error < StandardError; end
   # Your code goes here...
 end
+
+class Matr
+  attr_accessor :m
+
+  def initialize(m:)
+    @m = m
+  end
+
+  def [](index)
+    m[index]
+  end
+
+  def []=(index, value)
+    @m[index] = value
+  end
+
+  def determinant
+    # для матриц 2х2
+    m[0][0] * m[1][1] - m[0][1] * m[1][0]
+  end
+
+end
+
+
+# x = Matr.new(m: [[1,2,3],[4,5,6],[7,8,9]])
+# p x
+# p x[0]
+# p x[1][0]
+
+c = Matr.new(m: [[1,2],[4,5]])
+p c.determinant
