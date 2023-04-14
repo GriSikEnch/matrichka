2850350be7d961023b0c41703050ba6f0b944e23
Ksenia <106336275+sikorskayaX@users.noreply.github.com>
2023-04-14 14:31:52 +0000

    Add matrix multiplication

--- lib/matrichka.rb
+++ lib/matrichka.rb
@@ -9,6 +9,8 @@ end
 
 class Matr
   attr_accessor :m
+  attr_reader :rows
+  protected :rows
 
   def initialize(m:)
     @m = m
@@ -27,7 +29,23 @@ class Matr
     m[0][0] * m[1][1] - m[0][1] * m[1][0]
   end
 
-end
+  # Returns the number of rows
+  def row_count
+    @rows.size
+  end
+
+
+# Matrix multiplication.
+  def *(m) # m is matrix or vector or number
+      m_rows = m.rows
+      new_rows = rows.map do |row_i|
+        Array.new(m.column_count) do |j|
+          v[i][j] = 0
+          column_count.times do |k|
+            v[i][j] += row_i[k] * m_rows[k][j]
+          end
+          v[i][j]
+  end
 
 
 # x = Matr.new(m: [[1,2,3],[4,5,6],[7,8,9]])
