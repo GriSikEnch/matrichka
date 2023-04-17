495489596b1e5fee765d61f6091f9a8f3de3d6a0
NataGri <108687793+NataGri@users.noreply.github.com>
2023-04-17 09:59:29 +0000

    Add system linear equation Matrix method

--- lib/matrichka.rb
+++ lib/matrichka.rb
@@ -70,4 +70,85 @@ class Matr
       + m0[2] * m1[0] * m2[1] - m0[2] * m1[1] * m2[0]
     end
   end
+
+  
+  # Нахождение максимально элемента в строке
+  def max_element(col)
+    m = @matr
+    max_el = m[col][col]
+    max_row = col
+    m.each_with_index do |element,k|
+      element.each_with_index do |el,i|
+        if (el >= max_el)
+          max_el = el
+          max_row = k
+        end
+      end
+    end
+
+    if ( max_row != col )
+      m = swap(max_row,col)
+    end
+    #return max_el, max_row
+    
+  end
+
+  # Перестановка строк
+  def swap(max_row,col)
+    m = @matr
+    m.each_with_index do |element,i|
+      t = m[max_row][i]
+      m[max_row][i] = m[col][i]
+      m[col][i] = t
+
+    end
+
+  end
+# Вычетание строк
+  def sub(i,j,h)
+    m =@matr
+    m.each_with_index do |element,k|
+      m[i][k] -= m[j][k]*h
+
+    end
+  end
+# Приведение к треугольной матрице
+  def triangle
+    m = @matr
+
+    m.each_with_index do |element,i|
+      m = m.max_element(i)
+      (element+1).each_with_index do |el,j|
+        h = m[i][j]/m[j][j]
+        sub(i,j,h)
+      end
+    end
+  end
+
+  def gauss
+    m = @matr
+    r = @row_count
+
+    p m
+
+    m = m.triangle
+    det = m.determinant
+    s = Array.new(r){|n|n+1}
+
+    if (det == 0)
+      puts "The matrix is degenerate. The determinant is zero"
+    else
+      n = m.size - 1 
+      solution = Array.new(n, 0)
+      n.downto(-1)  {|i|
+        solution[i] = m[i][n] / m[i][i]
+        (i - 1).downto(-1) {|j|
+          p j
+          m[j][n] -= m[j][i] * solution[i]
+        }
+      }
+      solution
+    end
+  end
+
 end
