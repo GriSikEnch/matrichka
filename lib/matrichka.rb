842f9f500a2aba89301bcb655a3ce698427a1e83
NataGri <nata123456789021@gmail.com>
2023-04-19 21:11:00 +0300

    Remove unnecessary methods

--- lib/matrichka.rb
+++ lib/matrichka.rb
@@ -6,6 +6,11 @@ module MatrichkaExceptions
       super
     end
   end
+  class CantEvalDetErr < StandardError
+    def initialize(msg="Невозожно посчитать детерминант матрицы порядка выше, чем 3!!!")
+      super
+    end
+  end
 end
 
 
@@ -83,42 +88,14 @@ class Matr
       + m0[0] * m1[1] * m2[2] - m0[0] * m1[2] * m2[1] \
       - m0[1] * m1[0] * m2[2] + m0[1] * m1[2] * m2[0] \
       + m0[2] * m1[0] * m2[1] - m0[2] * m1[1] * m2[0]
+    else
+      raise CantEvalDetErr
     end
   end
 
   
   # Нахождение максимально элемента в строке
-  def max_element(col)
-    m = @matr
-    max_el = m[col][col]
-    max_row = col
-    m.each_with_index do |element,k|
-      element.each_with_index do |el,i|
-        if (el >= max_el)
-          max_el = el
-          max_row = k
-        end
-      end
-    end
-
-    if ( max_row != col )
-      m = swap(max_row,col)
-    end
-    #return max_el, max_row
-    
-  end
-
-  # Перестановка строк
-  def swap(max_row,col)
-    m = @matr
-    m.each_with_index do |element,i|
-      t = m[max_row][i]
-      m[max_row][i] = m[col][i]
-      m[col][i] = t
-
-    end
-
-  end
+  
 # Вычетание строк
   def sub(i,j,h)
     m =@matr
@@ -127,43 +104,11 @@ class Matr
 
     end
   end
-# Приведение к треугольной матрице
-  def triangle
-    m = @matr
 
-    m.each_with_index do |element,i|
-      m = m.max_element(i)
-      (element+1).each_with_index do |el,j|
-        h = m[i][j]/m[j][j]
-        sub(i,j,h)
-      end
-    end
-  end
-
-  def gauss
-    m = @matr
-    r = @row_count
-
-    p m
-
-    m = m.triangle
-    det = m.determinant
-    s = Array.new(r){|n|n+1}
-
-    if (det == 0)
-      puts "The matrix is degenerate. The determinant is zero"
-    else
-      n = m.size - 1 
-      solution = Array.new(n, 0)
-      n.downto(-1)  {|i|
-        solution[i] = m[i][n] / m[i][i]
-        (i - 1).downto(-1) {|j|
-          p j
-          m[j][n] -= m[j][i] * solution[i]
-        }
-      }
-      solution
-    end
   end
 
 end
+N = Matr.new([[1, 2], [3, 5]])
+p N.triangle
+
+
